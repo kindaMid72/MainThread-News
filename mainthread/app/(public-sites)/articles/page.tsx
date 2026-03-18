36c0b908852d33f9b4a86fdc0afde6fdba94d7d3
King Ahmad <kingahmadilyas03@gmail.com>
2026-03-18 10:39:26 +0800

    changed for good

--- mainthread/app/(public-sites)/articles/page.tsx
+++ mainthread/app/(public-sites)/articles/page.tsx
@@ -2,21 +2,21 @@ import AllArticlePage from "@/features/public-sites/all-article/AllArticlePage";
 
 
 import api from '@/libs/axiosInterceptor/axiosPublicInterceptor'
-import {cache} from 'react';
+import { cache } from 'react';
 
-import {Metadata} from 'next';
+import { Metadata } from 'next';
 
-export async function generateMetadata({searchParams}: {searchParams: Promise<{page: string, limit?: string}>}): Promise<Metadata> {
+export async function generateMetadata({ searchParams }: { searchParams: Promise<{ page: string, limit?: string }> }): Promise<Metadata> {
     let { page = '1', limit = '10' } = await searchParams;
-    if(!page || !limit){
+    if (!page || !limit) {
         page = '1';
-        limit = '12';
+        limit = '10';
     }
     return {
         title: "MainThread Articles | Page " + page,
         description: "All Articles in MainThread News | Page " + page,
         robots: {
-            index: page == '1'? true : false, // index only first page
+            index: page == '1' ? true : false, // index only first page
             follow: true,
         },
     };
@@ -27,9 +27,9 @@ const getAllArticles = cache(async (page: number, limit?: number) => {
     const response = await api.get(`/api/public/get-all-articles?page=${page}&limit=${limitValue}`);
     return response.data;
 })
-export default async function AllArticles({searchParams}: {searchParams: Promise<{page: string, limit?: string}>}) {
+export default async function AllArticles({ searchParams }: { searchParams: Promise<{ page: string, limit?: string }> }) {
     // TODO: implement ssr for all article page, config url first
-    let page: number  = Number((await searchParams).page || 1); 
+    let page: number = Number((await searchParams).page || 1);
     let limit: number | undefined = Number((await searchParams).limit || 10);
 
     const articles = await getAllArticles(page, limit);
